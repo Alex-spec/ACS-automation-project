e9042b4c3aa84c64aae3607f3772934378fe7c69
Alex Spec <alexyankovskio@gmail.com>
2025-03-11 18:09:27 +0100

    Добавлены базовые методы страниц

---
+++ base/base_methods.py
@@ -0,0 +1,22 @@
+class Base:
+    """ Базовый класс, содержащий универсальные методы """
+    def __init__(self, driver):
+        self.driver = driver
+
+    def get_current_url(self):
+        """Метод проверки url"""
+        get_url = self.driver.current_url
+        print("current url " + get_url)
+
+    def assert_url(self, result):
+        """Проверка корректности URL"""
+        get_url = self.driver.current_url
+        print(get_url)
+        assert result == get_url
+        print("--Корректная URL--")
+
+    def assert_word_sign_in(self, word, result):
+        """Проверка значения текста"""
+        value_word = word.text
+        assert value_word == result
+        print("Вход выполнен успешно")
